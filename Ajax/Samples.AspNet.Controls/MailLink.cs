1a83eaad0a41e3ced6d2b3f1157c5e0257312059
Christian Storb <christian.storb@outlook.com>
2018-07-17 22:08:46 +0200

    Samples.AspNet.Controls added

---
+++ Ajax/Samples.AspNet.Controls/MailLink.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Windows.Forms;
+
+namespace Samples.AspNet.Controls
+{
+    [
+    AspNetHostingPermission(SecurityAction.Demand, Level = AspNetHostingPermissionLevel.Minimal),
+    AspNetHostingPermission(SecurityAction.InheritanceDemand, Level= AspNetHostingPermissionLevel.Minimal),
+    DefaultProperty("Email"),
+    ParseChildren(true, "Text"),
+    ToolboxData("<{0}:MailLink runat=\"server\"> </{0}:MailLink>")
+    ]
+    public class MailLink : WebControl
+    {
+        [
+        Bindable(true),
+        Category("Appearance"),
+        DefaultValue(""),
+        Description("The e-mail address.")
+        ]
+        public virtual string Email
+        {
+            get
+            {
+                string s = (string)ViewState["Email"];
+                return s ?? String.Empty;
+            }
+            set
+            {
+                ViewState["Email"] = value;
+            }
+        }
+
+        [
+        Bindable(true),
+        Category("Appearance"),
+        DefaultValue(""),
+        Description("The text to display on the link."),
+        Localizable(true),
+        PersistenceMode(PersistenceMode.InnerDefaultProperty)
+        ]
+        public virtual string Text
+        {
+            get
+            {
+                string s = (string)ViewState["Text"];
+                return s ?? String.Empty;
+            }
+            set
+            {
+                ViewState["Text"] = value;
+            }
+        }
+
+        protected override HtmlTextWriterTag TagKey
+        {
+            get
+            {
+                return HtmlTextWriterTag.A;
+            }
+        }
+
+        protected override void AddAttributesToRender(
+            HtmlTextWriter writer)
+        {
+            base.AddAttributesToRender(writer);
+            writer.AddAttribute(HtmlTextWriterAttribute.Href,
+                "mailto:" + Email);
+        }
+
+        protected override void RenderContents(HtmlTextWriter writer)
+        {
+            if (Text == String.Empty)
+            {
+                Text = Email;
+            }
+            writer.WriteEncodedText(Text);
+        }
+    }
+
+}
---
+++ Ajax/WebSite/Pages/MailLink.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Title="MailLink" MasterPageFile="~/Content.master" Language="C#" AutoEventWireup="true" CodeFile="MailLink.aspx.cs" Inherits="Pages_MailLink" %>
+
+<asp:Content ID="HeadContent" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="BodyContent" ContentPlaceHolderID="body" runat="server">
+    <div class="well well-lg">
+        <h1><%: Title %></h1>
+    </div>
+    <div class="row">
+        <div class="col-md-12">        
+            <asp:HyperLink ID="HyperLink1" runat="server" NavigateUrl="https://msdn.microsoft.com/en-us/library/f820d25y.aspx" Target="_blank">Web Control Rendering Example</asp:HyperLink>
+        </div>
+        <div class="col-md-12">
+            <aspSample:MailLink id="MailLink1" Font-Bold="true" ForeColor="Green" Email="someone@example.com" runat="server">
+                Mail Webmaster
+            </aspSample:MailLink>
+        </div>
+    </div>
+
+</asp:Content>
+<asp:Content ID="FooterContent" ContentPlaceHolderID="footer" runat="server">
+</asp:Content>
